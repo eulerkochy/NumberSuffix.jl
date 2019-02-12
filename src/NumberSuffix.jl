5ed924ae59701c9dfcec6e70d1829ea3448df286
Koustav Chowdhury <kc99.kol@gmail.com>
2019-02-12 17:19:52 +0530

    created suffix for an integer

--- src/NumberSuffix.jl
+++ src/NumberSuffix.jl
@@ -1,5 +1,26 @@
 module NumberSuffix
 
-greet() = print("Hello World!")
+    export SuffixIt
 
-end # module
+    function SuffixIt(num)::String
+        if (typeof(num)!=Int64)
+            throw(TypeError(num,"cannot suffix this type of argument",Int64))
+        end
+        if (num<0)
+            throw(DomainError(x, "argument must be nonnegative"))
+        end
+        suffixedNumber=string(num)
+        lastDigit=num%10
+        if (lastDigit==0 || lastDigit>=4)
+            suffixedNumber=suffixedNumber*"th"
+        elseif (lastDigit==1)
+            suffixedNumber=suffixedNumber*"st"
+        elseif (lastDigit==2)
+            suffixedNumber=suffixedNumber*"nd"
+        else
+            suffixedNumber=suffixedNumber*"rd"
+        end
+        return suffixedNumber
+    end
+
+end
